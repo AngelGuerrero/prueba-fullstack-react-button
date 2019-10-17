9069e9d09626b7278d52b0378732a5e25e0e5720
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2019-10-16 20:20:10 -0500

    Adding backend files

---
+++ backend/src/routes/tests.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+let router = express.Router();
+
+router.get('/:value', (req, res) => {
+  let value = req.params.value;
+
+  let newValue = value.toUpperCase();
+
+  return res.status(200).json({ message: newValue });
+});
+
+module.exports = router;
